06286cb70db52551338162f895a6a4d728f3241a
Lesley Chang <lesley.hanna98@gmail.com>
2021-09-30 09:54:55 -0400

    fix gitifnore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,20 @@
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function Home() {
-  return <div>HOME</div>
+  return (
+    <Container>
+      <h1>HOME</h1>
+      <WalletMultiButton />
+    </Container>
+  );
 }
